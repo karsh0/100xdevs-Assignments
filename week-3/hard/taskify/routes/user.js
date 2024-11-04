caf56c729e2ce976ee086717e0a8fa8a4245a347
Karan <karansharma192004@gmail.com>
2024-11-04 18:37:37 +0530

    toggle updated

--- week-3/hard/taskify/routes/user.js
+++ week-3/hard/taskify/routes/user.js
@@ -5,7 +5,6 @@ const { TaskModel } = require('../db');
 
 userRouter.get('/', async (req,res)=>{
     const tasks = await TaskModel.find({});
-    console.log(tasks)
     res.render('index',{tasks})
 })
 
